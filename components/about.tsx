561adcc8f1566b987fb26f61d1254c4f4f61e7f8
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-21 20:54:13 -0700

    transition added to div

--- components/about.tsx
+++ components/about.tsx
@@ -6,7 +6,19 @@ type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div 
+      initial={{
+        opacity: 0
+      }}
+      whileInView={{
+        opacity: 1
+      }}
+      transition={{
+        duration: 1.5
+      }}
+    
+    
+    className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-yellow-400 text-2xl'>About</h3>
 
         <motion.img 
@@ -26,11 +38,11 @@ export default function About({}: Props) {
         />
 
         <div className='space-y-10 px-0 md:px-10'>
-          <h4 className='text-4xl font-semibold'>Here os a{" "}<span className='underline decoration-yellow-400'>Little</span>{" "} background</h4>
+          <h4 className='text-4xl font-semibold'>Here is a{" "}<span className='underline decoration-yellow-400'>Little</span>{" "} background</h4>
           <p className='text-sm'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam laboriosam velit sit, nisi officiis itaque, inventore, odit reiciendis impedit perspiciatis cum ratione possimus. Dolore id cumque delectus obcaecati qui dignissimos?
           </p>
         </div>
-    </div>
+    </motion.div>
   )
 }
