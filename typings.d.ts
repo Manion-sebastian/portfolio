ab9fce176f2a8cc7914b8f743af39cec44d01e90
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-22 14:01:07 -0700

    types set

--- typings.d.ts
+++ typings.d.ts
@@ -13,6 +13,30 @@ interface Image {
     }
 }
 
+export interface Technology extends SanityBody {
+    _type: "skill"
+    image: Image
+    progress: number
+    title: string
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill"
+    image: Image
+    progress: number
+    title: string
+}
+
+export interface Project extends SanityBody {
+    title: string
+    _type: "project"
+    image: Image
+    linkToBuild: string
+    summary: string
+    technologies: Technology[]
+
+}
+
 export interface PageInfo extends SanityBody {
     _type: 'pageInfo'
     address: string
