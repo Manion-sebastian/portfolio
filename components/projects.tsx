a0248a8fc39435d1de90fba513c149b38d981969
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-21 21:05:13 -0700

    added proxies to empty sections.

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,6 +4,10 @@ type Props = {}
 
 export default function Projects({}: Props) {
   return (
-    <div>projects</div>
+    <div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-yellow-400 text-2xl'>
+            standin
+        </h3>
+    </div>
   )
 }
