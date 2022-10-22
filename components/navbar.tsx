9320301bb9160b581ac55f1b86faf5519fcdf3eb
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-21 18:47:55 -0700

    first animation done!

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,8 +5,8 @@ export default function NavBar() {
 
     return (
         <nav>
-            <Link href='/'>Home</Link>
-            <Link href='/about'>About Me</Link>
+            {/* <Link href='/'>Home</Link>
+            <Link href='/about'>About Me</Link> */}
         </nav>
     )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -3,17 +3,11 @@ import Image from 'next/image'
 
 export default function About() {
     return (
-        <div className='bg-white'>
+        <div className=''>
             <h1 className={styles.extraRed} >Hello world</h1>
-
-            {/* load an image locally */}
-            <Image src='/vercel.svg' width={300} height={300} />
-
-            <Image src={'https://placekitten.com/400/500'}
-                alt={'prortforlo'}
-                width={400}
-                height={500}
-                />
+            
+            
+            
 
         </div>
     )
