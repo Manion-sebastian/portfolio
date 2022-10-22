0035485d5ff4091e181e1d34798f72293320d3f1
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-21 20:43:59 -0700

    about photo working

--- components/about.tsx
+++ components/about.tsx
@@ -10,7 +10,19 @@ export default function About({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-yellow-400 text-2xl'>About</h3>
 
         <motion.img 
+          initial={{
+            x:-200,
+            opacity: 0
+          }}
+          whileInView={{
+            x:0,
+            opacity: 1
+          }}
+          transition={{
+            duration:1.3
+          }}
             src='https://i.imgur.com/z5ffaRx.jpg'
+            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
         />
     </div>
   )
--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,7 @@ export default function Hero({}: Props) {
         <BackgroundCircles />
         <Image src='https://media-exp1.licdn.com/dms/image/C5603AQFKVY3ljseKuQ/profile-displayphoto-shrink_800_800/0/1660083494530?e=1671667200&v=beta&t=v-Znxg-d_QXgopWPvjNme3ivOvzE_3ezREUD5R0F0j0' alt='sebastian manion' width={150} height={150} className='rounded-full relative mx-auto' />
         <div className='z-20'>
-            <h2 className='text-sm uppercase text-gray-200 pb-2 tracking-[14px] text-yellow-400'>Software Engineer</h2>
+            <h2 className='text-sm uppercase pb-2 tracking-[14px] text-yellow-400'>Software Engineer</h2>
             <h1 className='text-5xl lg:text-6xl font-bold px-10'>
                 <span className='mr-3'>{text}</span>
                 <Cursor cursorColor='gold' />
