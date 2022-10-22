148021e26d5bb576c08747a9448ca508c3cb0dd9
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-21 19:44:01 -0700

    whitelisted websites

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import { Cursor, useTypewriter } from 
+'react-simple-typewriter'
+import BackgroundCircles from './backgroundCircles'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -15,7 +18,9 @@ export default function Hero({}: Props) {
     })
 
   return (
-    <div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+        <Image href= />
         <h1>
             <span>{text}</span>
             <Cursor cursorColor='gold' />
