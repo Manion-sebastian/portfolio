a7e0f19e0406113cce0d3ec789d51f314bea1b58
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-23 17:17:49 -0700

    broke scroll on sm else good.

--- components/about.tsx
+++ components/about.tsx
@@ -25,7 +25,7 @@ export default function About({ pageInfo }: Props) {
     
       
     
-    className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center '>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-yellow-400 text-2xl'>About</h3>
 
         <motion.img 
